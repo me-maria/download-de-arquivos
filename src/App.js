96b4796eedc35afc3c0b3d37865e607765a9b2de
Maria Eduarda <mariaeduardacorrea.mg@gmail.com>
2023-06-13 20:19:58 -0300

    estilização adicionada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import { useState } from "react";
+import './styles.css'
 
 function App() {
   const urlToDownload = 'http://speed.hetzner.de/100MB.bin'
   const [download, setDownload] = useState('')
   const [count, setCount] = useState(0)
   return (
-    <div className="App">
-      <h1>
+    <div className="App">  
+      <h1 className="title">
          Download com React
       </h1>
-      <p>
-        <button onClick={()=> {
+      <div className="container">
+      <div   className= "btn">
+        <button  onClick=
+        {()=> {
          setDownload(urlToDownload);
          setCount(oud => oud + 1);
-         }}>Download
+        }}>Download
         </button>
-      </p>
+      </div>
+      </div>
       <p>{download}</p>
-      {download && <iframe src= {download +'?c='+count} style={{display:'none'}}></iframe>}
+      <div className="container-iframe">
+        {download && <iframe src= {download +'?c='+count}></iframe>}
+      </div>
     </div>
   );
 }
